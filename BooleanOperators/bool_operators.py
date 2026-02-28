4477c7d6a7cfe202534c56407b9ea6b2151e6627
Gauravlovescode <gqurav69@gmail.com>
2026-02-28 17:07:37 +0545

    Add comparison and boolean operators examples in separate files

---
+++ BooleanOperators/bool_operators.py
@@ -0,0 +1,6 @@
+condition1 = True
+condition2 = False
+
+print(not condition1) #False
+print(condition1 and condition2) #False
+print(condition1 or condition2) #True
---
+++ ComparisonOperators/comp_operators.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+
+print(a == b) #false
+print(a != b) #true
+print(a > b) #false
+print(a <= b) #true
