3262a25f9c10fdea20b3c77662b69ef98e2aa84c
Gauravlovescode <gqurav69@gmail.com>
2026-02-28 17:01:18 +0545

    Add arithmetic and comparison operators examples in separate files

---
+++ AirthmeticsOperators/airth_operators.py
@@ -0,0 +1,11 @@
+1 + 1 #2
+2 - 1 #1
+2 * 2 #4
+4 / 2 #2
+4 % 3 #1
+4 ** 2 #16
+5 // 2 #2 divides and round up to the nearest whole number
+
+age = 8
+age += 8 # age = age + 8
+print(age)
--- DataTypes/checking_data_types.py
+++ DataTypes/checking_data_types.py
@@ -3,4 +3,10 @@ name = "Gaurav"
 print(isinstance(name, str)) # another way of checking if data type is string or not ( this can check other data types too)
 
 age = 19
-print(isinstance(age, int))
+print(isinstance(age, float)) # should print false
+
+days = float(321)
+print(isinstance(days, float)) # initially not a float but shall print true due to conversion
+
+number = int("12")
+print(isinstance(number, int)) # another example like line no. 8
