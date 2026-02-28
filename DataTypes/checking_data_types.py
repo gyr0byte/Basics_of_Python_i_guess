c1b8dd8502542a16b67aae3203549b9de9a19c55
Gauravlovescode <gqurav69@gmail.com>
2026-02-28 16:36:22 +0545

    Add checking_data_types.py to demonstrate type checking in Python

---
+++ DataTypes/checking_data_types.py
@@ -0,0 +1,6 @@
+name = "Gaurav"
+# print(type(name) == str) # first way to check if a variable is string or not
+print(isinstance(name, str)) # another way of checking if data type is string or not ( this can check other data types too)
+
+age = 19
+print(isinstance(age, int))
