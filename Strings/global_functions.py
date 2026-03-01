e0c2db8fd26d2df5634fcda3106403e00bd20683
Gauravlovescode <gqurav69@gmail.com>
2026-03-01 16:46:32 +0545

    Add boolean operations examples in boolean.py and string manipulation examples in global_functions.py

---
+++ Strings/global_functions.py
@@ -0,0 +1,7 @@
+name = "Gaurav"
+print(len(name))
+print("au" in name) # to check if any substrings contains in the main string
+
+print("Gaurav \" hehe ")
+print(name[3])
+print(name[1:5]) # 1 is inclusive and 4 is exclusive
