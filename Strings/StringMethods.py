8eec17f35dadfc9e96773230a78c019cca720728
Gauravlovescode <gqurav69@gmail.com>
2026-03-01 16:25:00 +0545

    Add string manipulation examples in StringMethods.py and concat.py

---
+++ Strings/StringMethods.py
@@ -0,0 +1,17 @@
+print("gaurav".upper())
+print("GAURAV".lower())
+
+print("i love tame impala".title()) # it will make the first letter of every word capital
+
+print("gaurav".islower()) # will check if the string is all lower case
+print("gg".isalpha()) # to check if string is only characters
+print("gqurav69".isalnum()) # to check if string contains character or digits
+
+print("gaurav".startswith("g")) 
+print("gaurav".endswith("n"))
+print("gaurav".replace("a", "q"))
+print("gqurav69@gmail.com".split("@"))
+print("     i am god hahha          ".strip()) # trim the white space at the end and the beginning
+print(" #".join("Gaurav")) # append the string
+
+print("Gaurav".find("r"))
---
+++ Strings/concat.py
@@ -0,0 +1,10 @@
+"Gaurav"
+'Gaurav' # two ways to write string
+name = "Gaurav"
+name += " is my name"
+print(name)
+
+# Multi line string
+print(""" Hello i am Gaurav
+      Hope you are doing well :)
+      """)
