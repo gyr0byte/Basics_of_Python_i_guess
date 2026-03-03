4f5663c1a9c7cf19db34e9f9e4bbbc5686ccc9d3
Gauravlovescode <gqurav69@gmail.com>
2026-03-03 17:32:52 +0545

    Add tuples.py with anime tuple operations and slicing examples

---
+++ Tuples/tuples.py
@@ -0,0 +1,10 @@
+animes = ("One Piece", "Naruto", "Solo Leveling")
+animes[1]
+animes.index("Naruto")
+len(animes)
+print("One Piece" in animes)
+#slicing tuple
+print(animes[1:3])
+print(sorted(animes))
+newanimes = animes + ("Black Clover", "Attack on Titan")
+print(newanimes)
