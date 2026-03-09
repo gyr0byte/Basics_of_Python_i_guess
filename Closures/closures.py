cd8f64b6a12eb62e5a3cfb67eb03206dcd77eb28
Gauravlovescode <gqurav69@gmail.com>
2026-03-09 09:04:21 +0545

    Add closures.py to demonstrate the use of nonlocal variables in a counter function

---
+++ Closures/closures.py
@@ -0,0 +1,13 @@
+def counter():
+    count = 0
+    
+    def increment():
+        nonlocal count
+        count += 1
+        return count
+    return increment
+
+increment = counter()
+print(increment())
+print(increment())
+print(increment())
