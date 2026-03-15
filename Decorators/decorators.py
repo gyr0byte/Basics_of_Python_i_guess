efd00eaa8eef6c1ed69fae1cf86b15b8c0c1a74a
Gauravlovescode <gqurav69@gmail.com>
2026-03-15 21:48:34 +0545

    Add logtime decorator example with hello function

---
+++ Decorators/decorators.py
@@ -0,0 +1,15 @@
+# Decorators
+
+def logtime(func):
+    def wrapper():
+        print("Before")
+        val = func()
+        print("After")
+        return val
+    return wrapper
+
+@logtime
+def hello():
+    print("Hello")
+
+hello()
