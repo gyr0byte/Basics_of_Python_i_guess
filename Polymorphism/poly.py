df40ebe924f874b5b936391362928f4826eecc39
Gauravlovescode <gqurav69@gmail.com>
2026-03-16 09:59:31 +0545

    Add Dog and Cat classes with eat methods in poly.py

---
+++ Polymorphism/poly.py
@@ -0,0 +1,15 @@
+class Dog:
+    def eat(self):
+        print("Dog is eating")
+
+
+class Cat:
+    def eat(self):
+        print("Cat is eating")
+
+
+animal1 = Dog()
+animal2 = Cat()
+
+animal1.eat()
+animal2.eat()
