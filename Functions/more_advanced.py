189dc3f09065a4ac988d1ac893a986121995be03
Gauravlovescode <gqurav69@gmail.com>
2026-03-18 22:47:19 +0545

    Refactor hello_func to use f-string formatting and add student_info function for variable argument handling

--- Functions/more_advanced.py
+++ Functions/more_advanced.py
@@ -1,5 +1,16 @@
 def hello_func(greeting):
-    return '{} Function.'.format(greeting)
+    return f"{greeting} Function."
 
 
 print(hello_func("Hi"))
+
+
+def student_info(*args, **kwargs):
+    print(args)  # these are like tuples
+    print(kwargs)  # these are like dictionaries
+
+
+course = ["Math", "Art"]
+info = {"name": "Gaurav", "age": 19}
+
+student_info(*course, **info)
