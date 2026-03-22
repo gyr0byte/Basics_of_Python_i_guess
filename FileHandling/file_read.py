934c982eaa8f5674ec91aa54e483dc59d9ec308f
Gauravlovescode <gqurav69@gmail.com>
2026-03-22 23:07:48 +0545

    Refactor file_read.py to implement chunked reading of file contents

--- FileHandling/file_read.py
+++ FileHandling/file_read.py
@@ -1,6 +1,15 @@
 filename = "FileHandling/test.txt"
 
 with open(filename, 'r') as f:
-    content = f.read()
-    print(content)
-
+    # content = f.read() # Reads the entire file at once
+    # content = f.readline() # Reads the file line by line
+    # print(content, end = "")
+    # content = f.readline()
+    # print(content)
+    # for line in f:
+    #     print(line, end = "") # this is efficient and memory usuage is low
+    size_to_read = 100
+    f_contents = f.read(size_to_read)
+    while len(f_contents) > 0:
+        print(f_contents, end = "")
+        f_contents = f.read(size_to_read)
