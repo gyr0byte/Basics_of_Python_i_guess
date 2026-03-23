6263d263022a0ad95cb79305cf765c6f3fa96260
Gauravlovescode <gqurav69@gmail.com>
2026-03-23 23:17:29 +0545

    Change CSV writer delimiter from '-' to tab in parse_csv.py

--- CSV_Module/parse_csv.py
+++ CSV_Module/parse_csv.py
@@ -5,7 +5,7 @@ with open(path, "r") as csv_file:
 
  #  next(csv_reader)  # Skip the header row
     with open("CSV_Module/new_names.csv", "w") as new_file:
-        csv_writer =  csv.writer(new_file, delimiter="-")
+        csv_writer =  csv.writer(new_file, delimiter="\t")
         for line in csv_reader:
             csv_writer.writerow(line)
     # for line in csv_reader:
