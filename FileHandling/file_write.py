193008ff074f72b95ce8dc63665af84c6ac7a9f5
Gauravlovescode <gqurav69@gmail.com>
2026-03-23 21:27:24 +0545

    Refactor file_write.py to copy contents from test2.txt to test2_copy.txt

--- FileHandling/file_write.py
+++ FileHandling/file_write.py
@@ -1,4 +1,6 @@
 filename = "FileHandling/test2.txt"
 
-with open(filename, 'w') as f:
-    f.write("This is the first line.\n")
+with open(filename, 'r') as rf:
+   with open("FileHandling/test2_copy.txt", "w") as wf:
+       for line in rf:
+           wf.write(line)
