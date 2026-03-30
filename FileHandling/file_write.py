b6216bcbd0262b3254b04e726e2f14b248e2eda9
Gauravlovescode <gqurav69@gmail.com>
2026-03-30 11:43:09 +0545

    Fix formatting in file_write.py by adding a newline at the end of the file write operation

--- FileHandling/file_write.py
+++ FileHandling/file_write.py
@@ -3,4 +3,4 @@ filename = "FileHandling/test2.txt"
 with open(filename, 'r') as rf:
    with open("FileHandling/test2_copy.txt", "w") as wf:
        for line in rf:
-           wf.write(line)
+           wf.write(line) 
